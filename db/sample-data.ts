0d73129f41aba42e90f96044775c177a37172307
stephen cleminshaw <stephencleminshaw@yahoo.co.uk>
2025-04-15 22:14:32 +0100

    commit

--- db/sample-data.ts
+++ db/sample-data.ts
@@ -1,4 +1,21 @@
+import {hashSync} from 'bcrypt-ts-edge';
+
+
 const sampleData = {
+  users:[
+   {
+    name: 'john',
+    email: 'admin@example.com',
+    password: hashSync('123456', 10),
+    role: 'admin'
+   },
+   {
+    name: 'jane',
+    email: 'user@example.com',
+    password: hashSync('123456', 10),
+    role: 'user',
+   }
+  ],
   products: [
     {
       name: 'Polo Sporting Stretch Shirt',
