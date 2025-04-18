9ed0f0a345d569e2d34d0fdad06e8b62e2cbd5d6
stephen cleminshaw <stephencleminshaw@yahoo.co.uk>
2025-04-18 22:58:31 +0100

    commit

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,4 +1,4 @@
-import { Card, CardHeader } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Metadata } from "next";
 import Link from "next/link";
 import Image from "next/image";
@@ -15,7 +15,10 @@ const SignInPage = () => {
          <Link href='/' className='flex-center'>
          <Image src='/images/logo.svg' width={100} height={100} alt={`${APP_NAME} logo`} priority={true}/>
          </Link>
+         <CardTitle className='text-center'>Sign In</CardTitle>
+         <CardDescription className='text-center'>Sign in to your account</CardDescription>
         </CardHeader>
+        <CardContent className='space-y-4'>{/* FORM HERE */ }</CardContent>
     </Card>
     </div>;
 };
