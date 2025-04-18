853bd5973fe62974e64daa6b1562211c562f374e
stephen cleminshaw <stephencleminshaw@yahoo.co.uk>
2025-04-18 17:55:05 +0100

    commit

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Sign In'
+}
+
+const SignInPage = () => {
+    return   <>Sign In </>;
+};
+ 
+export default SignInPage;
