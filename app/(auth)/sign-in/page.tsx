ef0183985194a3ffc9252af89089fd83d4dd8275
stephen cleminshaw <stephencleminshaw@yahoo.co.uk>
2025-04-18 22:20:47 +0100

    commit

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,11 +1,23 @@
+import { Card, CardHeader } from "@/components/ui/card";
 import { Metadata } from "next";
+import Link from "next/link";
+import Image from "next/image";
+import { APP_NAME } from "@/lib/constants";
 
 export const metadata: Metadata = {
     title: 'Sign In'
 }
 
 const SignInPage = () => {
-    return   <>Sign In </>;
+    return   <div className='w-full max-w-md mx-auto'> 
+    <Card>
+        <CardHeader className='space-y-4'>
+         <Link href='/' className='flex-center'>
+         <Image src='/images/logo.svg' width={100} height={100} alt={`${APP_NAME} logo`}/>
+         </Link>
+        </CardHeader>
+    </Card>
+    </div>;
 };
  
 export default SignInPage;
